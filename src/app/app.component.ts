2c5f7b760ec5aa368b0ec106156d89a5e3abebc3
dylan <dylanliner@gmail.com>
2021-03-26 23:44:04 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trello-app';
+
+  public toDos : Task[] = [{ 'title' : 'first task',
+  'description' : 'triplebyte Task'
+}];
+
+  public underDevs : Task[] = [{ 'title' : 'first task',
+  'description' : 'triplebyte Task'
+}];
+
+  public toTests : Task[] = [{ 'title' : 'first task',
+  'description' : 'triplebyte Task'
+}];
+
+
+public moveToUnderDev(selectedTask:Task){
+  this.toDos = this.toDos.filter(task =>task!==selectedTask);
+  this.underDevs.push(selectedTask)
+}
+
+public moveToToTest(selectedTask:Task){
+  this.underDevs = this.underDevs.filter(task =>task!==selectedTask);
+  this.toTests.push(selectedTask)
+}
+
+
+
+}
+
+export interface Task {
+  title : string,
+  description: string,
 }
